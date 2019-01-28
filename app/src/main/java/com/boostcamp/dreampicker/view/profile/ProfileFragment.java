77ac0994b7550fb1112819a707a06d94efdf1848
yeseul <sdc01194@gmail.com>
2019-01-28 17:43:56 +0900

    issue-022 프로필 피드 탭, 리스트 출력

--- app/src/main/java/com/boostcamp/dreampicker/view/profile/ProfileFragment.java
+++ app/src/main/java/com/boostcamp/dreampicker/view/profile/ProfileFragment.java
@@ -10,6 +10,9 @@ import com.boostcamp.dreampicker.view.BaseFragment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 public class ProfileFragment extends BaseFragment<FragmentProfileBinding> {
 
@@ -35,6 +38,34 @@ public class ProfileFragment extends BaseFragment<FragmentProfileBinding> {
     }
 
     private void initView() {
-        // TODO. view init
+
+        binding.viewPagerProfile.setAdapter(new ProfilePagerAdapter(getChildFragmentManager()));
+        binding.tabProfile.setupWithViewPager(binding.viewPagerProfile);
+
+        binding.tabProfile.getTabAt(0).setText("진행중인 투표");
+        binding.tabProfile.getTabAt(1).setText("완료된 투표");
+
+    }
+
+    class ProfilePagerAdapter extends FragmentPagerAdapter {
+
+        Fragment[] fragments = new Fragment[2];
+
+        public ProfilePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+            fragments[0] = FeedListFragment.getInstance();
+            fragments[1] = FeedListFragment.getInstance();
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
     }
 }
