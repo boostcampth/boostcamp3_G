b1f8a0a5c75613c74a7e08810d6f290dd3e58581
Yoon Young-jik <tbtzpdlql@gmail.com>
2019-01-28 16:31:49 +0900

    issue-009 임시 데이터 메인 피드에 표시

---
+++ app/src/main/java/com/boostcamp/dreampicker/viewmodel/FeedViewModel.java
@@ -0,0 +1,11 @@
+package com.boostcamp.dreampicker.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+
+public class FeedViewModel extends BaseViewModel {
+    public FeedViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
