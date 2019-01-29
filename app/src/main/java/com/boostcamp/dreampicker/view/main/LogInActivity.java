49a60d1b1fc4cb049775a7f423de8882bd2ab16c
yeseul <sdc01194@gmail.com>
2019-01-30 05:42:55 +0900

    issue-033 Login, Splash 수정

--- app/src/main/java/com/boostcamp/dreampicker/view/main/LogInActivity.java
+++ app/src/main/java/com/boostcamp/dreampicker/view/main/LogInActivity.java
@@ -1,27 +1,28 @@
 package com.boostcamp.dreampicker.view.main;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.boostcamp.dreampicker.R;
-import com.boostcamp.dreampicker.view.main.MainActivity;
+import com.boostcamp.dreampicker.databinding.ActivityLogInBinding;
+import com.boostcamp.dreampicker.view.BaseActivity;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends BaseActivity<ActivityLogInBinding> {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_log_in;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
 
-        try{
-            Thread.sleep(500);
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-        finish();
+        initView();
     }
+
+    private void initView() {
+
+        // TODO. 뷰 그리기
+    }
+
 }
--- app/src/main/java/com/boostcamp/dreampicker/view/main/SplashActivity.java
+++ app/src/main/java/com/boostcamp/dreampicker/view/main/SplashActivity.java
@@ -1,25 +1,31 @@
 package com.boostcamp.dreampicker.view.main;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.boostcamp.dreampicker.R;
-import com.boostcamp.dreampicker.view.main.MainActivity;
+import com.boostcamp.dreampicker.databinding.ActivitySplashBinding;
+import com.boostcamp.dreampicker.view.BaseActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity<ActivitySplashBinding> {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_splash;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
-        try{
-            Thread.sleep(500);
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }
+        // 2초후 액티비티 전환
+        new Handler().postDelayed(this::startMainActivity, 2000);
+    }
 
+    private void startMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
