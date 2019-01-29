7ef4abd1dc17e06dfd9030a567f27581f2635142
Yoon Young-jik <tbtzpdlql@gmail.com>
2019-01-29 16:33:37 +0900

    Merge branch 'develop' of https://github.com/boostcampth/boostcamp3_G into feature/issue-028-apply-databinding-feed
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/boostcamp/dreampicker/view/main/MainActivity.java
    #	app/src/main/java/com/boostcamp/dreampicker/viewmodel/BaseViewModel.java
    #	app/src/main/java/com/boostcamp/dreampicker/viewmodel/FeedViewModel.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/boostcamp/dreampicker/view/main/MainActivity.java
+++ app/src/main/java/com/boostcamp/dreampicker/view/main/MainActivity.java
@@ -1,11 +1,13 @@
 package com.boostcamp.dreampicker.view.main;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.boostcamp.dreampicker.R;
 import com.boostcamp.dreampicker.view.ResultFragment;
 import com.boostcamp.dreampicker.view.SearchFragment;
 import com.boostcamp.dreampicker.view.UpLoadFragment;
+import com.boostcamp.dreampicker.view.feed.FeedFragment;
 import com.boostcamp.dreampicker.view.profile.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
