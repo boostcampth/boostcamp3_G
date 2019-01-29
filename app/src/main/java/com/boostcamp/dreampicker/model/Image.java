d3848f57fef4c1dcfe31d37fa79d335d4d800a5b
yeseul <sdc01194@gmail.com>
2019-01-29 21:31:31 +0900

    issue-030 충돌해결

--- app/src/main/java/com/boostcamp/dreampicker/model/Image.java
+++ app/src/main/java/com/boostcamp/dreampicker/model/Image.java
@@ -1,38 +1,39 @@
 package com.boostcamp.dreampicker.model;
 
 public class Image {
-    private String id;
+    private String imageId;
     // TODO : 이후 URL로 변경 예정
-    private int resourceId;
-    private String[] tags;
+    private int image;
+    // TODO : 이후 TagGroup 적용
+    private String tag;
 
-    public Image(String id, int resourceId, String[] tags) {
-        this.id = id;
-        this.resourceId = resourceId;
-        this.tags = tags;
+    public Image(String id, int image, String tag) {
+        this.imageId = id;
+        this.image = image;
+        this.tag = tag;
     }
 
-    public String getId() {
-        return id;
+    public String getImageId() {
+        return imageId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setImageId(String imageId) {
+        this.imageId = imageId;
     }
 
-    public int getResourceId() {
-        return resourceId;
+    public int getImage() {
+        return image;
     }
 
-    public void setResourceId(int resourceId) {
-        this.resourceId = resourceId;
+    public void setImage(int image) {
+        this.image = image;
     }
 
-    public String[] getTags() {
-        return tags;
+    public String getTag() {
+        return tag;
     }
 
-    public void setTags(String[] tags) {
-        this.tags = tags;
+    public void setTag(String tag) {
+        this.tag = tag;
     }
 }
