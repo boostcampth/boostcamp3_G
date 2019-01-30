4fa93e5f9fd01628f0346780208fc390a8ba67ec
yeseul <sdc01194@gmail.com>
2019-01-30 16:10:16 +0900

    issue-039 data source 인터페이스추가

--- app/src/main/java/com/boostcamp/dreampicker/data/source/feed/FeedDataSource.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/source/feed/FeedDataSource.java
@@ -7,5 +7,10 @@ import java.util.List;
 import io.reactivex.Single;
 
 public interface FeedDataSource {
+
+    // 메인 피드 요청
     Single<List<Feed>> getAllFeed();
+
+    // 피드 검색 결과 요청
+    Single<List<Feed>> searchFeed(String searchKey);
 }
