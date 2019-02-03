596810c38432a67fdbf07d15afd384b81ce3a366
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-03 18:32:10 +0900

    issue-077 TedPermission 추가

--- app/src/main/java/com/boostcamp/dreampicker/view/upload/UploadActivity.java
+++ app/src/main/java/com/boostcamp/dreampicker/view/upload/UploadActivity.java
@@ -1,18 +1,23 @@
 package com.boostcamp.dreampicker.view.upload;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.widget.ImageView;
 
 import com.boostcamp.dreampicker.R;
 import com.boostcamp.dreampicker.databinding.ActivityUploadBinding;
+import com.boostcamp.dreampicker.utils.Util;
 import com.boostcamp.dreampicker.view.BaseActivity;
+import com.gun0912.tedpermission.TedPermissionResult;
+import com.tedpark.tedpermission.rx2.TedRx2Permission;
 
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
-import androidx.transition.TransitionManager;
+import io.reactivex.disposables.Disposable;
 
 public class UploadActivity extends BaseActivity<ActivityUploadBinding> {
     private boolean leftImageAnimationToggle = false;
@@ -23,6 +28,11 @@ public class UploadActivity extends BaseActivity<ActivityUploadBinding> {
     private ConstraintSet rootSet = new ConstraintSet();
     private ConstraintSet leftSet = new ConstraintSet();
     private ConstraintSet rightSet = new ConstraintSet();
+
+    private Disposable disposable;
+
+    private static final String CAMERA = Manifest.permission.CAMERA;
+    private static final String READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,22 +46,38 @@ public class UploadActivity extends BaseActivity<ActivityUploadBinding> {
         rootSet.clone(rootLayout);
         leftSet.clone(this, R.layout.activity_upload_detail_left);
         rightSet.clone(this, R.layout.activity_upload_detail_right);
+
+        ImageView leftImage = findViewById(R.id.iv_upload_left);
+        ImageView rightImage = findViewById(R.id.iv_upload_right);
+
+        leftImage.setOnClickListener((v) -> onImageClick(Util.LEFT));
+        rightImage.setOnClickListener((v) -> onImageClick(Util.RIGHT));
     }
 
-    public void onImageLeftClick(View view) {
-        TransitionManager.beginDelayedTransition(rootLayout);
+    public void onImageClick(@Util.VoteFlag int flag) {
+        disposable = TedRx2Permission.with(this)
+                .setPermissions(CAMERA, READ_EXTERNAL_STORAGE)
+                .request()
+                .filter(TedPermissionResult::isGranted)
+                .map(result -> flag)
+                .subscribe(this::showBottomPicker);
+    }
+
+    public void showBottomPicker(@Util.VoteFlag int flag) {
+        Log.d("Melon", flag + "");
+        /*TransitionManager.beginDelayedTransition(rootLayout);
         ConstraintSet set;
         set = !leftImageAnimationToggle ? leftSet : rootSet;
         set.applyTo(rootLayout);
-        leftImageAnimationToggle = !leftImageAnimationToggle;
+        leftImageAnimationToggle = !leftImageAnimationToggle;*/
     }
 
-    public void onImageRightClick(View view) {
-        TransitionManager.beginDelayedTransition(rootLayout);
+    public void onImageRightClick(@Util.VoteFlag int flag) {
+        /*TransitionManager.beginDelayedTransition(rootLayout);
         ConstraintSet set;
         set = !rightImageAnimationToggle ? rightSet : rootSet;
         set.applyTo(rootLayout);
-        rightImageAnimationToggle = !rightImageAnimationToggle;
+        rightImageAnimationToggle = !rightImageAnimationToggle;*/
     }
 
     public static Intent getLaunchIntent(Context context) {
@@ -62,5 +88,13 @@ public class UploadActivity extends BaseActivity<ActivityUploadBinding> {
     @Override
     protected int getLayoutId() {
         return R.layout.activity_upload;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(!disposable.isDisposed()) {
+            disposable.dispose();
+        }
     }
 }
