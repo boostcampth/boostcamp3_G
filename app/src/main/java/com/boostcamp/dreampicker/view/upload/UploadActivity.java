d39665a810943de79a03c1d37c8f811b2b67912a
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-03 16:47:08 +0900

    issue-050 업로드 레이아웃 애니메이션 추가

--- app/src/main/java/com/boostcamp/dreampicker/view/upload/UploadActivity.java
+++ app/src/main/java/com/boostcamp/dreampicker/view/upload/UploadActivity.java
@@ -3,14 +3,27 @@ package com.boostcamp.dreampicker.view.upload;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.boostcamp.dreampicker.R;
 import com.boostcamp.dreampicker.databinding.ActivityUploadBinding;
 import com.boostcamp.dreampicker.view.BaseActivity;
 
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.transition.TransitionManager;
 
 public class UploadActivity extends BaseActivity<ActivityUploadBinding> {
+    private boolean leftImageAnimationToggle = false;
+    private boolean rightImageAnimationToggle = false;
+
+    private ConstraintLayout rootLayout;
+
+    private ConstraintSet rootSet = new ConstraintSet();
+    private ConstraintSet leftSet = new ConstraintSet();
+    private ConstraintSet rightSet = new ConstraintSet();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +32,26 @@ public class UploadActivity extends BaseActivity<ActivityUploadBinding> {
     }
 
     private void initView() {
+        rootLayout = findViewById(R.id.cl_upload_root);
+        rootSet.clone(rootLayout);
+        leftSet.clone(this, R.layout.activity_upload_detail_left);
+        rightSet.clone(this, R.layout.activity_upload_detail_right);
+    }
 
+    public void onImageLeftClick(View view) {
+        TransitionManager.beginDelayedTransition(rootLayout);
+        ConstraintSet set;
+        set = !leftImageAnimationToggle ? leftSet : rootSet;
+        set.applyTo(rootLayout);
+        leftImageAnimationToggle = !leftImageAnimationToggle;
+    }
+
+    public void onImageRightClick(View view) {
+        TransitionManager.beginDelayedTransition(rootLayout);
+        ConstraintSet set;
+        set = !rightImageAnimationToggle ? rightSet : rootSet;
+        set.applyTo(rootLayout);
+        rightImageAnimationToggle = !rightImageAnimationToggle;
     }
 
     public static Intent getLaunchIntent(Context context) {
