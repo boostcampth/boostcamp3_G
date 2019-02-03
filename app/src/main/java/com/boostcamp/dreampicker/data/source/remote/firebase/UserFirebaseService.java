2490e385e451e04dc3590f8b8da65ff9677f867d
leedaegun <lkg1104@naver.com>
2019-02-03 19:00:50 +0900

    feature/issue-068-datasource-firebase userDataSource 쿼리구현

--- app/src/main/java/com/boostcamp/dreampicker/data/source/remote/firebase/UserFirebaseService.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/source/remote/firebase/UserFirebaseService.java
@@ -4,13 +4,28 @@ import com.boostcamp.dreampicker.R;
 import com.boostcamp.dreampicker.data.model.User;
 import com.boostcamp.dreampicker.data.model.UserDetail;
 import com.boostcamp.dreampicker.data.source.UserDataSource;
+import com.boostcamp.dreampicker.model.Feed;
+import com.boostcamp.dreampicker.model.User;
+import com.boostcamp.dreampicker.model.UserDetail;
+import com.boostcamp.dreampicker.model.UserInfo;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import androidx.annotation.NonNull;
 import io.reactivex.Single;
 
 public class UserFirebaseService implements UserDataSource {
+
+    private static final String COLLECTION_USERDETAIL = "userDetail";
+    private static final String COLLECTION_USER = "user";
 
     private static volatile UserFirebaseService INSTANCE;
 
@@ -53,19 +68,74 @@ public class UserFirebaseService implements UserDataSource {
     }
 
     @Override
-    public Single<UserDetail> getProfileUserDetail(String userId) {
-
-        return null;
+    public Single<UserInfo> getProfileUserDetail(String userId) {
+        //유저아이디로 Info찾기
+        return Single.create(emitter -> FirebaseFirestore.getInstance().collection(COLLECTION_USERDETAIL)
+                .document(userId)
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshot -> {
+                    emitter.onSuccess(queryDocumentSnapshot.toObject(UserInfo.class));
+                })
+                .addOnFailureListener(emitter::onError));
     }
 
     @Override
     public Single<List<User>> addProfileFollowingList(String userId, int pageIndex, int pageUnit) {
+        final List<User> followingList = new ArrayList<>();
+        return Single.create(emitter -> FirebaseFirestore.getInstance().collection(COLLECTION_USERDETAIL)
+                .document(userId)
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshot -> {
 
-        return null;
+                    Query query = FirebaseFirestore.getInstance().collection(COLLECTION_USER)
+                            .orderBy("name")
+                            .limit(pageUnit)
+                            .startAt(queryDocumentSnapshot);
+                    query.get()
+                            .addOnSuccessListener(queryDocumentSnapshots -> {
+                                Query next = FirebaseFirestore.getInstance().collection(COLLECTION_USER)
+                                        .orderBy("name")
+                                        .limit(pageUnit)
+                                        .startAt(queryDocumentSnapshot);
+                            })
+                            .addOnFailureListener(emitter::onError);
+                    emitter.onSuccess(followingList);
+                })
+                .addOnFailureListener(emitter::onError));
     }
 
     @Override
     public Single<List<User>> addProfileFollowerList(String userId, int pageIndex, int pageUnit) {
+        final List<User> followerList = new ArrayList<>();
+        return Single.create(emitter -> FirebaseFirestore.getInstance().collection(COLLECTION_USERDETAIL)
+                .document(userId)
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshot -> {
+
+                    Query query = FirebaseFirestore.getInstance().collection(COLLECTION_USER)
+                            .orderBy("name")
+                            .limit(pageUnit)
+                            .startAt(queryDocumentSnapshot);
+                    query.get()
+                            .addOnSuccessListener(queryDocumentSnapshots -> {
+                                Query next = FirebaseFirestore.getInstance().collection(COLLECTION_USER)
+                                    .orderBy("name")
+                                    .limit(pageUnit)
+                                    .startAt(queryDocumentSnapshot);
+                                next.get()
+                                        .addOnSuccessListener(queryDocumentSnapshots1 -> {
+
+                                })
+                                        .addOnFailureListener(emitter::onError);
+                            })
+                            .addOnFailureListener(emitter::onError);
+                    emitter.onSuccess(followerList);
+                })
+                .addOnFailureListener(emitter::onError));
+    }
+
+    @Override
+    public Single<UserDetail> getProfileUserDetail(String userId) {
 
         return null;
     }
