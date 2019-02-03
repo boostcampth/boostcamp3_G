5b5055b892b4240ffb552e0547be5acd4b2591d4
leedaegun <lkg1104@naver.com>
2019-02-03 18:59:52 +0900

    feature/issue-068-google-login feedDataSource 쿼리 구현 테스팅 필요

--- app/src/main/java/com/boostcamp/dreampicker/data/source/feed/FeedRepository.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/source/feed/FeedRepository.java
@@ -28,6 +28,43 @@ public class FeedRepository implements FeedDataSource {
 
     @Override
     public Single<List<Feed>> getAllFeed() {
+        // Todo : 테스트 후 삭제
+//
+//        List<Feed> feeds = new ArrayList<>();
+//
+//        Image image1 = new Image("image-0-up", R.drawable.image1, "카페");
+//        Image image2 = new Image("image-0-down", R.drawable.image2, "술집");
+//        User user1 = new User("user-0", "박신혜", R.drawable.profile);
+//
+//        Feed feed1 = new Feed(
+//                "feed-0",
+//                image1,
+//                image2,
+//                user1,
+//                "내일 소개남이랑 첫 데이트인데 장소 좀 골라주세요~!ㅎㅎ",
+//                "2018.01.28",
+//                false
+//        );
+//
+//        Image image3 = new Image("image-1-up", R.drawable.jajang, "짜장면");
+//        Image image4 = new Image("image-1-down", R.drawable.jambong, "짬뽕");
+//        User user2 = new User("user-1", "공유", R.drawable.profile2);
+//
+//        Feed feed2 = new Feed(
+//                "feed-0",
+//                image3,
+//                image4,
+//                user2,
+//                "짬뽕, 짜장면",
+//                "2019.01.29",
+//                false
+//        );
+//
+//        feeds.add(feed1);
+//        feeds.add(feed2);
+//        feeds.add(feed1);
+//
+//        return Single.just(feeds);
 
         // TODO : 테스팅 끝나면 사용
         return remoteDataSource.getAllFeed();
@@ -42,25 +79,26 @@ public class FeedRepository implements FeedDataSource {
 
     @Override
     public Single<List<Feed>> addMainFeedList(String userId, int pageIndex, int pageUnit) {
-
-        return remoteDataSource.addMainFeedList(userId, pageIndex, pageUnit);
+        return remoteDataSource.addMainFeedList( userId,  pageIndex,  pageUnit);
     }
 
     @Override
     public void updateFeedVote(String feedId, String userId, int voteFlag) {
-
         remoteDataSource.updateFeedVote(feedId, userId, voteFlag);
     }
 
     @Override
     public Single<List<Feed>> addSearchFeedList(String searchKey, int pageIndex, int pageUnit) {
+        return remoteDataSource.addSearchFeedList(searchKey,pageIndex,pageUnit);
+    }
 
-        return remoteDataSource.addSearchFeedList(searchKey, pageIndex, pageUnit);
+    @Override
+    public Single<List<Feed>> addProfileFeedList(String userId, int pageIndex, int pageUnit) {
+        return remoteDataSource.addProfileFeedList(userId,pageIndex,pageUnit);
     }
 
     @Override
     public void toggleFeedState(String feedId, boolean isEnded) {
-
-        remoteDataSource.toggleFeedState(feedId, isEnded);
+        remoteDataSource.toggleFeedState(feedId,isEnded);
     }
 }
