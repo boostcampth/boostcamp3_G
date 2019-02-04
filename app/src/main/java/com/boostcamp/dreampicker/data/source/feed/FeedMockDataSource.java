1dbac9f0ca5df6536882a4ea87c19c8dea9b7741
leedaegun <lkg1104@naver.com>
2019-02-04 16:35:50 +0900

    feature/issue-068-datasource-firebase 쿼리수정

--- app/src/main/java/com/boostcamp/dreampicker/data/source/feed/FeedMockDataSource.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/source/feed/FeedMockDataSource.java
@@ -1,13 +1,18 @@
 package com.boostcamp.dreampicker.data.source.feed;
 
+import android.util.Log;
+
 import com.boostcamp.dreampicker.R;
 import com.boostcamp.dreampicker.model.Feed;
 import com.boostcamp.dreampicker.model.Image;
 import com.boostcamp.dreampicker.model.User;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import io.reactivex.Single;
 
@@ -15,35 +20,47 @@ public class FeedMockDataSource implements FeedDataSource{
     @Override
     public Single<List<Feed>> getAllFeed() {
         List<Feed> feedList = new ArrayList<>();
-        Image image1 = new Image("image-0-up", R.drawable.image1, "카페");
-        Image image2 = new Image("image-0-down", R.drawable.image2, "술집");
-        User user1 = new User("user-0", "박신혜", R.drawable.profile);
-        Feed feed1 = new Feed(
-                "feed-0",
-                Arrays.asList(image1, image2),
-                user1,
-                "내일 소개남이랑 첫 데이트인데 장소 좀 골라주세요~!ㅎㅎ",
-                "2018.01.28",
-                false
-        );
-
-        Image image3 = new Image("image-1-up", R.drawable.jajang, "짜장면");
-        Image image4 = new Image("image-1-down", R.drawable.jambong, "짬뽕");
-        User user2 = new User("user-1", "공유", R.drawable.profile2);
-
-        Feed feed2 = new Feed(
-                "feed-1",
-                Arrays.asList(image3, image4),
-                user2,
-                "짬뽕, 짜장면",
-                "2019.01.29",
-                false
-        );
-
-        feedList.add(feed1);
-        feedList.add(feed2);
-
-        return Single.just(feedList);
+//        Image image1 = new Image("image-0-up", R.drawable.image1, "카페");
+//        Image image2 = new Image("image-0-down", R.drawable.image2, "술집");
+//        User user1 = new User("user-0", "박신혜", R.drawable.profile);
+//        Feed feed1 = new Feed(
+//                "feed-0",
+//                Arrays.asList(image1, image2),
+//                user1,
+//                "내일 소개남이랑 첫 데이트인데 장소 좀 골라주세요~!ㅎㅎ",
+//                "2018.01.28",
+//                false
+//        );
+//
+//        Image image3 = new Image("image-1-up", R.drawable.jajang, "짜장면");
+//        Image image4 = new Image("image-1-down", R.drawable.jambong, "짬뽕");
+//        User user2 = new User("user-1", "공유", R.drawable.profile2);
+//
+//        Feed feed2 = new Feed(
+//                "feed-1",
+//                Arrays.asList(image3, image4),
+//                user2,
+//                "짬뽕, 짜장면",
+//                "2019.01.29",
+//                false
+//        );
+//
+//        feedList.add(feed1);
+//        feedList.add(feed2);
+        return Single.create(emitter -> FirebaseFirestore.getInstance().collection("feed")
+                //.whereEqualTo("name","yeseul")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                            feedList.add(document.toObject(Feed.class));
+                        }
+                        emitter.onSuccess(feedList);
+                    } else {
+                        emitter.onError(task.getException());
+                    }
+                }));
+        //return Single.just(feedList);
     }
 
     @Override
--- app/src/main/java/com/boostcamp/dreampicker/data/source/user/UserRemoteDataSource.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/source/user/UserRemoteDataSource.java
@@ -6,6 +6,7 @@ import com.boostcamp.dreampicker.model.User;
 import com.boostcamp.dreampicker.model.UserDetail;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -42,25 +43,50 @@ public class UserRemoteDataSource implements UserDataSource {
     public Single<List<User>> searchAllUser(String searchKey) {
 
         // TODO. 임시 데이터
-        List<User> userList = new ArrayList<>();
-        User user = new User("" ,"yeseul",R.drawable.profile);
-        userList.add(user);
-        userList.add(user);
-        userList.add(user);
-        userList.add(user);
+        final List<User> userList = new ArrayList<>();
+//        User user = new User("" ,"yeseul",R.drawable.profile);
+//        userList.add(user);
+//        userList.add(user);
+//        userList.add(user);
+//        userList.add(user);
+        return Single.create(emitter -> FirebaseFirestore.getInstance().collection(COLLECTION_USERDETAIL)
+                .whereEqualTo("name","yeseul")
+                .get()
+                .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                    userList.add(document.toObject(User.class));
+                                    emitter.onSuccess(userList);
+                                }
+                            } else {
+                                emitter.onError(task.getException());
+                            }
+                        }));
 
-        return Single.just(userList);
+                        //return Single.just(userList);
     }
 
     @Override
     public Single<UserDetail> getUserInfo(String userId) {
 
         // TODO. 임시 데이터
-        UserDetail user = new UserDetail();
-        user.setName("yeseul");
-        user.setProfileImageUrl("https://img.sbs.co.kr/newimg/news/20170622/201061239_1280.jpg");
-
-        return Single.just(user);
+        final UserDetail user = new UserDetail();
+//        user.setName("yeseul");
+//        user.setProfileImageUrl("https://img.sbs.co.kr/newimg/news/20170622/201061239_1280.jpg");
+//
+//        return Single.just(user);
+        return Single.create(emitter -> FirebaseFirestore.getInstance().collection(COLLECTION_USERDETAIL)
+                .whereEqualTo("name","yeseul")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                            emitter.onSuccess(document.toObject(UserDetail.class));
+                        }
+                    } else {
+                        emitter.onError(task.getException());
+                    }
+                }));
     }
 
     @Override
@@ -103,29 +129,29 @@ public class UserRemoteDataSource implements UserDataSource {
     @Override
     public Single<List<User>> addProfileFollowerList(String userId, int pageIndex, int pageUnit) {
         final List<User> followerList = new ArrayList<>();
+        //TODO : 일단 다가져오고 페이징 추가
         return Single.create(emitter -> FirebaseFirestore.getInstance().collection(COLLECTION_USERDETAIL)
                 .document(userId)
                 .get()
                 .addOnSuccessListener(queryDocumentSnapshot -> {
+                    UserDetail user = queryDocumentSnapshot.toObject(UserDetail.class);
+                    if (user != null) {
+                        emitter.onSuccess(user.getFollowerList());
+                    }
+//                    Query next = FirebaseFirestore.getInstance().collection(COLLECTION_USER)
+//                            .orderBy("name")
+//                            .limit(pageUnit)
+//                            .startAfter(pageIndex);
+//                            next.get()
+//                                .addOnSuccessListener(queryDocumentSnapshots -> {
+//                                    for (DocumentSnapshot document : Objects.requireNonNull(queryDocumentSnapshots.getDocuments())) {
+//                                        followerList.add(document.toObject(User.class));
+//                                    }
+//                                    emitter.onSuccess(followerList);
+//                                })
+//                                .addOnFailureListener(emitter::onError);
 
-                    Query query = FirebaseFirestore.getInstance().collection(COLLECTION_USER)
-                            .orderBy("name")
-                            .limit(pageUnit)
-                            .startAt(queryDocumentSnapshot);
-                    query.get()
-                            .addOnSuccessListener(queryDocumentSnapshots -> {
-                                Query next = FirebaseFirestore.getInstance().collection(COLLECTION_USER)
-                                    .orderBy("name")
-                                    .limit(pageUnit)
-                                    .startAt(queryDocumentSnapshot);
-                                next.get()
-                                        .addOnSuccessListener(queryDocumentSnapshots1 -> {
 
-                                })
-                                        .addOnFailureListener(emitter::onError);
-                            })
-                            .addOnFailureListener(emitter::onError);
-                    emitter.onSuccess(followerList);
                 })
                 .addOnFailureListener(emitter::onError));
     }
@@ -135,17 +161,39 @@ public class UserRemoteDataSource implements UserDataSource {
 
     @Override
     public Single<List<User>> addSearchUserList(String searchKey, int pageIndex, int pageUnit) {
+        // TODO : 완성된 쿼리 테스트 필요
         List<User> userList = new ArrayList<>();
 
-        for(int i = 0 ; i < pageUnit ; i++) {
-            User user = new User("userId",
-                    "사용자 " + (i+1),
-                    "https://img.sbs.co.kr/newimg/news/20170622/201061239_1280.jpg",
-                    R.drawable.profile);
+//        for(int i = 0 ; i < pageUnit ; i++) {
+//            User user = new User("userId",
+//                    "사용자 " + (i+1),
+//                    "https://img.sbs.co.kr/newimg/news/20170622/201061239_1280.jpg",
+//                    R.drawable.profile);
+//
+//            userList.add(user);
+//        }
+// return Single.just(userList);
+        return Single.create(emitter -> FirebaseFirestore.getInstance().collection(COLLECTION_USER)
+                .whereEqualTo("name",searchKey)
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshot -> {
+                    DocumentSnapshot lastVisible = queryDocumentSnapshot.getDocuments()
+                            .get(queryDocumentSnapshot.size() -1);
+                    Query next = FirebaseFirestore.getInstance().collection(COLLECTION_USER)
+                            .orderBy("name")
+                            .limit(pageUnit)
+                            .startAt(lastVisible);
+                    next.get()
+                           .addOnSuccessListener(task -> {
+                               for (DocumentSnapshot document : Objects.requireNonNull(task.getDocuments())) {
+                                   userList.add(document.toObject(User.class));
+                               }
+                               emitter.onSuccess(userList);
 
-            userList.add(user);
-        }
-
-        return Single.just(userList);
+                           })
+                           .addOnFailureListener(emitter::onError);
+                    emitter.onSuccess(userList);
+                })
+                .addOnFailureListener(emitter::onError));
     }
 }
