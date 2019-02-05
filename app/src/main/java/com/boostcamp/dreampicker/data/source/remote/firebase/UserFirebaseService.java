e82e1163c63ecac6261587807a7be8683e348c5d
leedaegun <lkg1104@naver.com>
2019-02-05 16:28:19 +0900

    Merge branch 'feature/issue-068-datasource-firebase' of https://github.com/boostcampth/boostcamp3_G into feature/issue-068-datasource-firebase
    # Conflicts:
    #	app/src/main/java/com/boostcamp/dreampicker/data/source/feed/FeedRemoteDataSource.java
    #	app/src/main/java/com/boostcamp/dreampicker/data/source/feed/FeedRepository.java
    #	app/src/main/java/com/boostcamp/dreampicker/data/source/local/test/FeedMockDataSource.java
    #	app/src/main/java/com/boostcamp/dreampicker/data/source/user/UserRemoteDataSource.java

--- app/src/main/java/com/boostcamp/dreampicker/data/source/remote/firebase/UserFirebaseService.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/source/remote/firebase/UserFirebaseService.java
@@ -38,56 +38,7 @@ public class UserFirebaseService implements UserDataSource {
         return INSTANCE;
     }
 
-    @Override
-    public Single<List<User>> searchAllUser(String searchKey) {
 
-        // TODO. 삭제
-        final List<User> userList = new ArrayList<>();
-//        User user = new User("" ,"yeseul",R.drawable.profile);
-//        userList.add(user);
-//        userList.add(user);
-//        userList.add(user);
-//        userList.add(user);
-
-        //return Single.just(userList);
-        return Single.create(emitter -> FirebaseFirestore.getInstance().collection(COLLECTION_USERDETAIL)
-                .whereEqualTo("name",searchKey)
-                .get()
-                .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
-                                    userList.add(document.toObject(User.class));
-                                }
-                                emitter.onSuccess(userList);
-                            } else {
-                                emitter.onError(task.getException());
-                            }
-                        }));
-
-    }
-
-    @Override
-    public Single<UserDetail> getUserInfo(String userId) {
-
-        // TODO. 삭제
-        final UserDetail user = new UserDetail();
-//        user.setName("yeseul");
-//        user.setProfileImageUrl("https://img.sbs.co.kr/newimg/news/20170622/201061239_1280.jpg");
-//
-//        return Single.just(user);
-        return Single.create(emitter -> FirebaseFirestore.getInstance().collection(COLLECTION_USERDETAIL)
-                .whereEqualTo("id","userId")
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
-                            emitter.onSuccess(document.toObject(UserDetail.class));
-                        }
-                    } else {
-                        emitter.onError(task.getException());
-                    }
-                }));
-    }
 
     @Override
     public Single<UserDetail> getProfileUserDetail(String userId) {
