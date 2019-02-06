a7d13130faccb0b384f2172298608b8414f48cc3
leedaegun <lkg1104@naver.com>
2019-02-06 17:09:57 +0900

    feature/issue-065-google-login 구글 로그인 기능 구현

--- app/src/main/java/com/boostcamp/dreampicker/presentation/main/LogInActivity.java
+++ app/src/main/java/com/boostcamp/dreampicker/presentation/main/LogInActivity.java
@@ -3,68 +3,54 @@ package com.boostcamp.dreampicker.presentation.main;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import com.boostcamp.dreampicker.R;
 import com.boostcamp.dreampicker.databinding.ActivityLogInBinding;
 import com.boostcamp.dreampicker.presentation.BaseActivity;
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-import androidx.annotation.NonNull;
 
-public class LogInActivity extends BaseActivity<ActivityLogInBinding>implements GoogleApiClient.OnConnectionFailedListener {
+public class LogInActivity extends BaseActivity<ActivityLogInBinding>{
 
     private static final int RC_SIGN_IN = 200;
+
     private FirebaseAuth mAuth;
     private GoogleSignInClient mGoogleSignInClient;
-    private GoogleApiClient mGoogleApiClient;
-    public static Intent getLaunchIntent(Context context) {
 
-        return new Intent(context, LogInActivity.class);
-    }
+
+    public static Intent getLaunchIntent(Context context) { return new Intent(context, LogInActivity.class); }
 
     @Override
-    protected int getLayoutId() {
-        return R.layout.activity_log_in;
-    }
+    protected int getLayoutId() { return R.layout.activity_log_in; }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        initView();
         GoogleSignInOptions gso = new GoogleSignInOptions
                 .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("")
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         mAuth = FirebaseAuth.getInstance();
+        initView();
 
     }
 
     private void initView() {
-
-        // TODO. 뷰 그리기
-        binding.signInButton.setSize(SignInButton.SIZE_STANDARD);
-        binding.signInButton.setOnClickListener(v -> {
+        binding.btnSignIn.setSize(SignInButton.SIZE_STANDARD);
+        binding.btnSignIn.setOnClickListener(v -> {
             Intent signInIntent = mGoogleSignInClient.getSignInIntent();
             startActivityForResult(signInIntent, RC_SIGN_IN);
         });
@@ -74,25 +60,17 @@ public class LogInActivity extends BaseActivity<ActivityLogInBinding>implements 
     protected void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null) startMainActivity();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == RC_SIGN_IN) {
-
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
-                if (account != null) {
-                    firebaseAuthWithGoogle(account);
-                }
-            } catch (ApiException e) {
-                e.printStackTrace();
-
-            }
-
-
+                if (account != null) { firebaseAuthWithGoogle(account); }
+            } catch (ApiException e) { e.printStackTrace(); }
         }
     }
 
@@ -100,16 +78,16 @@ public class LogInActivity extends BaseActivity<ActivityLogInBinding>implements 
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(),null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, task -> {
-                    if(!task.isSuccessful()){
+                    if(task.isSuccessful()){
+                        // TODO : 사용자 정보 로딩
                         FirebaseUser user = mAuth.getCurrentUser();
-                        Toast.makeText(LogInActivity.this, "인증 실패", Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(LogInActivity.this, "구글 로그인 인증 성공", Toast.LENGTH_SHORT).show();
-                    }
+                        startMainActivity();
+                    }else{ task.addOnFailureListener(Throwable::printStackTrace); }
                 });
     }
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+    private void startMainActivity() {
+        startActivity(MainActivity.getLaunchIntent(this));
+        finish();
     }
+
 }
