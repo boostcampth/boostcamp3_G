21f53ab8386fdbc73f089e23a25605a4af7cf985
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-07 09:22:11 +0900

    issue-099 BaseFragment 업데이트

--- app/src/main/java/com/boostcamp/dreampicker/presentation/feed/detail/FeedDetailLeftFragment.java
+++ app/src/main/java/com/boostcamp/dreampicker/presentation/feed/detail/FeedDetailLeftFragment.java
@@ -1,46 +1,41 @@
 package com.boostcamp.dreampicker.presentation.feed.detail;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.boostcamp.dreampicker.R;
 import com.boostcamp.dreampicker.databinding.FragmentFeedDetailImageBinding;
+import com.boostcamp.dreampicker.presentation.BaseFragment;
 import com.boostcamp.dreampicker.utils.GlideApp;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
-public class FeedDetailLeftFragment extends Fragment {
-   private FragmentFeedDetailImageBinding binding;
+public class FeedDetailLeftFragment extends BaseFragment<FragmentFeedDetailImageBinding> {
 
    public FeedDetailLeftFragment() {
 
    }
 
-   public static Fragment newInstance() {
+   static Fragment newInstance() {
        return new FeedDetailLeftFragment();
    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_feed_detail_image, container, false);
-        return binding.getRoot();
-    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Test
         GlideApp.with(this)
                 .load(R.drawable.jajang)
                 .transform(new RoundedCorners(20))
                 .into(binding.ivFeedDetailImage);
     }
 
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_feed_detail_image;
+    }
 }
--- app/src/main/java/com/boostcamp/dreampicker/presentation/feed/detail/FeedDetailRightFragment.java
+++ app/src/main/java/com/boostcamp/dreampicker/presentation/feed/detail/FeedDetailRightFragment.java
@@ -1,45 +1,42 @@
 package com.boostcamp.dreampicker.presentation.feed.detail;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.boostcamp.dreampicker.R;
 import com.boostcamp.dreampicker.databinding.FragmentFeedDetailImageBinding;
+import com.boostcamp.dreampicker.presentation.BaseFragment;
 import com.boostcamp.dreampicker.utils.GlideApp;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
-public class FeedDetailRightFragment extends Fragment {
-    private FragmentFeedDetailImageBinding binding;
+public class FeedDetailRightFragment extends BaseFragment<FragmentFeedDetailImageBinding> {
 
     public FeedDetailRightFragment() {
 
     }
 
-    public static Fragment newInstance() {
+    static Fragment newInstance() {
         return new FeedDetailRightFragment();
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_feed_detail_image, container, false);
-        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+        // Todo
         GlideApp.with(this)
                 .load(R.drawable.skin)
                 .transform(new RoundedCorners(20))
                 .into(binding.ivFeedDetailImage);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_feed_detail_image;
     }
 }
