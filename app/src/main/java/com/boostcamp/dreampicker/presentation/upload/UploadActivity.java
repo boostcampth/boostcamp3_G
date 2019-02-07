de0e9199921e3941eeab32414effc88ce0de7d2f
leedaegun <lkg1104@naver.com>
2019-02-07 18:52:45 +0900

    feature/issue-101-upload-feed upload viewModel 수정

--- app/src/main/java/com/boostcamp/dreampicker/presentation/upload/UploadActivity.java
+++ app/src/main/java/com/boostcamp/dreampicker/presentation/upload/UploadActivity.java
@@ -8,8 +8,6 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.boostcamp.dreampicker.R;
-import com.boostcamp.dreampicker.data.source.local.test.ProfileMockDataSource;
-import com.boostcamp.dreampicker.data.source.local.test.UploadMockDataSource;
 import com.boostcamp.dreampicker.data.source.remote.firebase.FeedFirebaseService;
 import com.boostcamp.dreampicker.data.source.remote.firebase.UserFirebaseService;
 import com.boostcamp.dreampicker.data.source.repository.FeedRepository;
@@ -49,8 +47,6 @@ public class UploadActivity extends BaseActivity<ActivityUploadBinding> {
                         FeedRepository.getInstance(FeedFirebaseService.getInstance()),
                         UserRepository.getInstance(UserFirebaseService.getInstance())))
                 .get(UploadViewModel.class);
-
-        // final UploadViewModel viewModel = new UploadViewModel(new UploadMockDataSource(), new ProfileMockDataSource());
 
         binding.setViewModel(viewModel);
         binding.setLifecycleOwner(this);
--- app/src/main/java/com/boostcamp/dreampicker/presentation/upload/UploadViewModelFactory.java
+++ app/src/main/java/com/boostcamp/dreampicker/presentation/upload/UploadViewModelFactory.java
@@ -2,8 +2,6 @@ package com.boostcamp.dreampicker.presentation.upload;
 
 import com.boostcamp.dreampicker.data.source.FeedDataSource;
 import com.boostcamp.dreampicker.data.source.UserDataSource;
-import com.boostcamp.dreampicker.data.source.local.test.ProfileMockDataSource;
-import com.boostcamp.dreampicker.data.source.local.test.UploadMockDataSource;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
@@ -11,11 +9,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 public class UploadViewModelFactory implements ViewModelProvider.Factory {
     private final FeedDataSource feedRepository;
-    private final UserDataSource userRepositry;
+    private final UserDataSource userRepository;
 
     UploadViewModelFactory(FeedDataSource feedRepository, UserDataSource userRepositry) {
         this.feedRepository = feedRepository;
-        this.userRepositry = userRepositry;
+        this.userRepository = userRepositry;
     }
 
     @NonNull
@@ -24,8 +22,7 @@ public class UploadViewModelFactory implements ViewModelProvider.Factory {
         if(modelClass.isAssignableFrom(UploadViewModel.class)) {
             //noinspection unchecked
             // Todo : 인터페이스 변경시 수정
-            return (T) new UploadViewModel(new UploadMockDataSource(), new ProfileMockDataSource());
-            //return (T) new UploadViewModel(feedRepository, userRepositry);
+            return (T) new UploadViewModel(feedRepository, userRepository);
         } else {
             throw new IllegalArgumentException("ViewModel Not Found");
         }
