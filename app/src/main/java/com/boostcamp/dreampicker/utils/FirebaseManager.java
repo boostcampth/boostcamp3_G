806852121a12bf0b3a969641af65ee0cd0686ad7
yeseul <sdc01194@gmail.com>
2019-02-08 02:55:42 +0900

    issue-117 코드정리

--- app/src/main/java/com/boostcamp/dreampicker/utils/FirebaseManager.java
+++ app/src/main/java/com/boostcamp/dreampicker/utils/FirebaseManager.java
@@ -7,18 +7,9 @@ import androidx.annotation.Nullable;
 
 public class FirebaseManager {
 
-    /**
-     * ID 만 필요한 곳에서 사용됨 */
     @Nullable
-    public static String getCurrentUserId(){
+    public static String getCurrentUserId() {
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         return user == null ? null : user.getUid();
-    }
-
-    /**
-     * 내 프로필 화면에서 사용됨 */
-    @Nullable
-    public static FirebaseUser getCurrentUser(){
-        return FirebaseAuth.getInstance().getCurrentUser();
     }
 }
