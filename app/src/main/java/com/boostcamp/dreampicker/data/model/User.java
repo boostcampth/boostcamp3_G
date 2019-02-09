22d2dc71ee4046d2cacab60ffd5318a8f9ff00a8
leedaegun <lkg1104@naver.com>
2019-02-10 03:16:09 +0900

    feature/issue-149-design-upload-interface user클래스 생성

--- app/src/main/java/com/boostcamp/dreampicker/data/model/User.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/model/User.java
@@ -4,53 +4,24 @@ public class User {
     private String id; // 유저 아이디
     private String name; // 유저 이름
     private String profileImageUrl; // 유저 프로필 사진 url
-    // TODO : 이후 url로 변경
-    private int profileImageResource; // 유저 임시 사진?
 
     public User() { }
 
-    public User(String id, String name, int profileImageResource) {
-        this.id = id;
-        this.name = name;
-        this.profileImageResource = profileImageResource;
-    }
-
-    public User(String id, String name, String profileImageUrl, int profileImageResource) {
+    public User(String id, String name, String profileImageUrl) {
         this.id = id;
         this.name = name;
         this.profileImageUrl = profileImageUrl;
-        this.profileImageResource = profileImageResource;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getProfileImageUrl() {
         return profileImageUrl;
-    }
-
-    public void setProfileImageUrl(String profileImageUrl) {
-        this.profileImageUrl = profileImageUrl;
-    }
-
-    public int getProfileImageResource() {
-        return profileImageResource;
-    }
-
-    public void setProfileImageResource(int profileImageResource) {
-        this.profileImageResource = profileImageResource;
     }
 }
