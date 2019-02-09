ca961f264a167538d40b19e3890f2f956fb51aee
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-09 18:18:00 +0900

    issue-145 데이터 모델에 맞도록 최적화 및 주석 추가, 유저 아이템 제거

--- app/src/main/java/com/boostcamp/dreampicker/data/model/FeedItem.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/model/FeedItem.java
@@ -7,51 +7,51 @@ import androidx.annotation.Nullable;
 
 public class FeedItem {
     @NonNull
-    private String id; // 피드 ID : 해당 피드를 구별할 수 있는 키로 사용됩니다.
+    private final String id; // 피드 ID : 해당 피드를 구별할 수 있는 키로 사용됩니다.
     @NonNull
-    private String imageUrlA; // 1번 이미지 URL
+    private final String imageUrlA; // 1번 이미지 URL
     @NonNull
-    private String imageUrlB; // 2번 이미지 URL
+    private final String imageUrlB; // 2번 이미지 URL
     @Nullable
-    private List<String> tagListA; // 1번 이미지 태그 리스트
+    private final List<String> tagListA; // 1번 이미지 태그 리스트
     @Nullable
-    private List<String> tagListB; // 2번 이미지 태그 리스트
+    private final List<String> tagListB; // 2번 이미지 태그 리스트
     @NonNull
-    private UserItem userItem; // 작성자 정보
+    private final User user; // 작성자 정보
     @NonNull
-    private String content; // 본문
+    private final String content; // 본문
     @NonNull
-    private String date; // 업로드 일자
+    private final String date; // 업로드 일자
 
-    private int countA; // 1번 투표 수
-    private int countB; // 2번 투표 수
-    private int voteFlag; // 본인 투표 위치
+    private final int countA; // 1번 투표 수
+    private final int countB; // 2번 투표 수
 
-    public FeedItem() { }
+    // [0] : 미투표 || [1] : 1번 투표 || [2] : 2번 투표
+    private final int selection; // 본인 투표 위치
 
     public FeedItem(@NonNull String id,
                     @NonNull String imageUrlA,
                     @NonNull String imageUrlB,
                     @Nullable List<String> tagListA,
                     @Nullable List<String> tagListB,
-                    @NonNull UserItem userItem,
+                    @NonNull User user,
                     @NonNull String content,
                     @NonNull String date,
                     int countA,
                     int countB,
-                    int voteFlag) {
+                    int selection) {
 
         this.id = id;
         this.imageUrlA = imageUrlA;
         this.imageUrlB = imageUrlB;
         this.tagListA = tagListA;
         this.tagListB = tagListB;
-        this.userItem = userItem;
+        this.user = user;
         this.content = content;
         this.date = date;
         this.countA = countA;
         this.countB = countB;
-        this.voteFlag = voteFlag;
+        this.selection = selection;
     }
 
     @NonNull
@@ -80,8 +80,8 @@ public class FeedItem {
     }
 
     @NonNull
-    public UserItem getUserItem() {
-        return userItem;
+    public User getUser() {
+        return user;
     }
 
     @NonNull
@@ -102,7 +102,7 @@ public class FeedItem {
         return countB;
     }
 
-    public int getVoteFlag() {
-        return voteFlag;
+    public int getSelection() {
+        return selection;
     }
 }
