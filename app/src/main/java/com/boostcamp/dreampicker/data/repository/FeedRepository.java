ec45431d16d46603260a5ba588038c72a54c93f5
yeseul <sdc01194@gmail.com>
2019-02-09 22:42:00 +0900

    issue-155 업로드 피드 정보 요청 인터페이스

---
+++ app/src/main/java/com/boostcamp/dreampicker/data/repository/FeedRepository.java
@@ -0,0 +1,22 @@
+package com.boostcamp.dreampicker.data.repository;
+
+import com.boostcamp.dreampicker.data.model.ProfileFeed;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import io.reactivex.Single;
+
+public interface FeedRepository {
+
+    /**
+     * 유저가 업로드 한 투표 목록 페이징
+     *
+     * @param userId 유저 ID
+     * @param start 시작 index
+     * @param display 페이지 size
+     * @return 피드 리스트 stream
+     */
+    @NonNull
+    Single<List<ProfileFeed>> getProfileFeed(@NonNull String userId, int start, int display);
+}
