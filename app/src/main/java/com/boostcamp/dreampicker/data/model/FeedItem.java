0e9447b96b46f9874d7df29cc9d1ceaed810fbff
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-09 18:04:38 +0900

    issue-145 불필요한 필드 삭제, 필드명 최적화

--- app/src/main/java/com/boostcamp/dreampicker/data/model/FeedItem.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/model/FeedItem.java
@@ -13,9 +13,9 @@ public class FeedItem {
     @NonNull
     private String imageUrlB; // 2번 이미지 URL
     @Nullable
-    private List<String> tagA; // 1번 이미지 태그 리스트
+    private List<String> tagListA; // 1번 이미지 태그 리스트
     @Nullable
-    private List<String> tagB; // 2번 이미지 태그 리스트
+    private List<String> tagListB; // 2번 이미지 태그 리스트
     @NonNull
     private UserItem userItem; // 작성자 정보
     @NonNull
@@ -25,7 +25,6 @@ public class FeedItem {
 
     private int countA; // 1번 투표 수
     private int countB; // 2번 투표 수
-    private int totalCount; // 총합 투표 수
     private int voteFlag; // 본인 투표 위치
 
     public FeedItem() { }
@@ -33,27 +32,25 @@ public class FeedItem {
     public FeedItem(@NonNull String id,
                     @NonNull String imageUrlA,
                     @NonNull String imageUrlB,
-                    @Nullable List<String> tagA,
-                    @Nullable List<String> tagB,
+                    @Nullable List<String> tagListA,
+                    @Nullable List<String> tagListB,
                     @NonNull UserItem userItem,
                     @NonNull String content,
                     @NonNull String date,
                     int countA,
                     int countB,
-                    int totalCount,
                     int voteFlag) {
 
         this.id = id;
         this.imageUrlA = imageUrlA;
         this.imageUrlB = imageUrlB;
-        this.tagA = tagA;
-        this.tagB = tagB;
+        this.tagListA = tagListA;
+        this.tagListB = tagListB;
         this.userItem = userItem;
         this.content = content;
         this.date = date;
         this.countA = countA;
         this.countB = countB;
-        this.totalCount = totalCount;
         this.voteFlag = voteFlag;
     }
 
@@ -73,13 +70,13 @@ public class FeedItem {
     }
 
     @Nullable
-    public List<String> getTagA() {
-        return tagA;
+    public List<String> getTagListA() {
+        return tagListA;
     }
 
     @Nullable
-    public List<String> getTagB() {
-        return tagB;
+    public List<String> getTagListB() {
+        return tagListB;
     }
 
     @NonNull
@@ -103,10 +100,6 @@ public class FeedItem {
 
     public int getCountB() {
         return countB;
-    }
-
-    public int getTotalCount() {
-        return totalCount;
     }
 
     public int getVoteFlag() {
