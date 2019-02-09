7154ef99e9b8f13de67c3281be0f456084fa57d3
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-10 02:53:20 +0900

    issue-160 FeedRemoteData, FeedRemoteVoteItem 추가

---
+++ app/src/main/java/com/boostcamp/dreampicker/data/source/firestore/model/FeedRemoteData.java
@@ -0,0 +1,69 @@
+package com.boostcamp.dreampicker.data.source.firestore.model;
+
+import com.boostcamp.dreampicker.data.model.User;
+
+import java.util.Date;
+import java.util.Map;
+
+public class FeedRemoteData {
+    private String id;
+    private FeedRemoteVoteItem itemA;
+    private FeedRemoteVoteItem itemB;
+    private User user;
+    private String content;
+    private Date date;
+    private Map<String, String> votedUserMap;
+    private boolean isEnded;
+
+    public FeedRemoteData() { }
+
+    public FeedRemoteData(String id,
+                          FeedRemoteVoteItem itemA,
+                          FeedRemoteVoteItem itemB,
+                          User user,
+                          String content,
+                          Map<String, String> votedUserMap,
+                          Date date,
+                          boolean isEnded) {
+        this.id = id;
+        this.itemA = itemA;
+        this.itemB = itemB;
+        this.user = user;
+        this.content = content;
+        this.date = date;
+        this.votedUserMap = votedUserMap;
+        this.isEnded = isEnded;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public FeedRemoteVoteItem getItemA() {
+        return itemA;
+    }
+
+    public FeedRemoteVoteItem getItemB() {
+        return itemB;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Map<String, String> getVotedUserMap() {
+        return votedUserMap;
+    }
+
+    public boolean isEnded() {
+        return isEnded;
+    }
+}
---
+++ app/src/main/java/com/boostcamp/dreampicker/data/source/firestore/model/FeedRemoteVoteItem.java
@@ -0,0 +1,31 @@
+package com.boostcamp.dreampicker.data.source.firestore.model;
+
+import java.util.List;
+
+public class FeedRemoteVoteItem {
+    private String id;
+    private String imageUrl;
+    private List<String> tagList;
+
+    public FeedRemoteVoteItem() { }
+
+    public FeedRemoteVoteItem(String id,
+                              String imageUrl,
+                              List<String> tagList) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.tagList = tagList;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public List<String> getTagList() {
+        return tagList;
+    }
+}
