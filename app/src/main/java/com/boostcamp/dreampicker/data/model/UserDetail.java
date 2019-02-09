1995d963f3da75af4bdb4b56a6830e0f1db1f47e
yeseul <sdc01194@gmail.com>
2019-02-09 21:39:52 +0900

    issue-155 프로필 화면에 보여줄 data

--- app/src/main/java/com/boostcamp/dreampicker/data/model/UserDetail.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/model/UserDetail.java
@@ -2,69 +2,64 @@ package com.boostcamp.dreampicker.data.model;
 
 import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 public class UserDetail {
+    @NonNull
     private String id; // 유저 아이디
+    @NonNull
     private String name; // 유저 이름
-    private String email; // 유저 이메일
+    @Nullable
     private String profileImageUrl; // 유저 프로필 url
     private int feedCount; // 올린 피드 수
     private int followerCount; // 팔로워 수
     private int followingCount; // 팔로잉 수
-    private boolean isFollow; // 내가 이 사람을 팔로우 했는지 여부
+    private boolean isFollow; // 내가 팔로우 한 유저인지 여부
+    private List<ProfileFeed> feedList; // 해당 유저의 피드 리스트
 
-    private List<User> followerList; // 해당 유저의 팔로워 리스트
-    private List<User> followingList; // 해당 유저의 팔로잉 리스트
-    private List<LegacyFeed> feedList; // 해당 유저의 피드 리스트
-
-    public UserDetail() { }
-
-    public UserDetail(String id,
-                      String name,
-                      String email,
-                      String profileImageUrl,
+    public UserDetail(@NonNull String id,
+                      @NonNull String name,
+                      @Nullable String profileImageUrl,
                       int feedCount,
                       int followerCount,
                       int followingCount,
-                      boolean isFollow) {
+                      boolean isFollow,
+                      List<ProfileFeed> feedList) {
         this.id = id;
         this.name = name;
-        this.email = email;
         this.profileImageUrl = profileImageUrl;
         this.feedCount = feedCount;
         this.followerCount = followerCount;
         this.followingCount = followingCount;
         this.isFollow = isFollow;
+        this.feedList = feedList;
     }
 
+    @NonNull
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(@NonNull String id) {
         this.id = id;
     }
 
+    @NonNull
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(@NonNull String name) {
         this.name = name;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+    @Nullable
     public String getProfileImageUrl() {
         return profileImageUrl;
     }
 
-    public void setProfileImageUrl(String profileImageUrl) {
+    public void setProfileImageUrl(@Nullable String profileImageUrl) {
         this.profileImageUrl = profileImageUrl;
     }
 
@@ -100,27 +95,11 @@ public class UserDetail {
         isFollow = follow;
     }
 
-    public List<User> getFollowerList() {
-        return followerList;
-    }
-
-    public void setFollowerList(List<User> followerList) {
-        this.followerList = followerList;
-    }
-
-    public List<User> getFollowingList() {
-        return followingList;
-    }
-
-    public void setFollowingList(List<User> followingList) {
-        this.followingList = followingList;
-    }
-
-    public List<LegacyFeed> getFeedList() {
+    public List<ProfileFeed> getFeedList() {
         return feedList;
     }
 
-    public void setFeedList(List<LegacyFeed> feedList) {
+    public void setFeedList(List<ProfileFeed> feedList) {
         this.feedList = feedList;
     }
 }
