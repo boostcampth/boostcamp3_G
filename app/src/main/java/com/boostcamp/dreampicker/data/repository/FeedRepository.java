527f796b14ebb9166a98f774db34c14fb6d0d2df
leedaegun <lkg1104@naver.com>
2019-02-09 23:00:31 +0900

    feature/issue-148-design-upload-interface 경로 및 rename

--- app/src/main/java/com/boostcamp/dreampicker/data/repository/FeedRepository.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/repository/FeedRepository.java
@@ -1,11 +1,11 @@
-package com.boostcamp.dreampicker.data.source.repository;
+package com.boostcamp.dreampicker.data.repository;
 
 import com.boostcamp.dreampicker.data.model.FeedUploadRequest;
 
 import androidx.annotation.NonNull;
 import io.reactivex.Completable;
 
-public interface FeedUploadRequestRepository {
+public interface FeedRepository {
 
     // [업로드] 사용자가 작성한 피드를 서버에 업로드한다.
     @NonNull
