ae4576610aa2d7f174b2250dc5ed680373c3f778
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-09 20:09:36 +0900

    issue-145 아이템 의미에 맞도록 ID 추가

--- app/src/main/java/com/boostcamp/dreampicker/data/model/VoteSelectionItem.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/model/VoteSelectionItem.java
@@ -7,15 +7,19 @@ import androidx.annotation.Nullable;
 
 public class VoteSelectionItem {
     @NonNull
-    private final String id; // 이미지 URL
+    private final String id; // 아이템 ID
+    @NonNull
+    private final String imageUrl; // 이미지 URL
     @Nullable
     private final List<String> tagList; // 이미지 태그 리스트
     private final int voteCount; // 해당 이미지 투표 유저 수
 
     public VoteSelectionItem(@NonNull String id,
+                             @NonNull String imageUrl,
                              @Nullable List<String> tagList,
                              int voteCount) {
         this.id = id;
+        this.imageUrl = imageUrl;
         this.tagList = tagList;
         this.voteCount = voteCount;
     }
@@ -23,6 +27,11 @@ public class VoteSelectionItem {
     @NonNull
     public String getId() {
         return id;
+    }
+
+    @NonNull
+    public String getImageUrl() {
+        return imageUrl;
     }
 
     @Nullable
