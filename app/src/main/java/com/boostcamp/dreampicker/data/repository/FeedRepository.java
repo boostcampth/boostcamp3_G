8719dc455ba309fca353e91f34617a1b36a7ba48
yeseul <sdc01194@gmail.com>
2019-02-10 00:27:43 +0900

    issue-155 startAt 파라미터 수정

--- app/src/main/java/com/boostcamp/dreampicker/data/repository/FeedRepository.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/repository/FeedRepository.java
@@ -2,6 +2,7 @@ package com.boostcamp.dreampicker.data.repository;
 
 import com.boostcamp.dreampicker.data.model.ProfileFeed;
 
+import java.util.Date;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -13,10 +14,10 @@ public interface FeedRepository {
      * 유저가 업로드 한 투표 목록 페이징
      *
      * @param userId 유저 ID
-     * @param startAt 시작 index
-     * @param pageSize 페이지 size
+     * @param endBefore 이 값의 다음 데이터를 요청
+     * @param pageSize 페이지의 size
      * @return 피드 리스트 stream
      */
     @NonNull
-    Single<List<ProfileFeed>> getProfileFeedList(@NonNull final String userId, final int startAt, final int pageSize);
+    Single<List<ProfileFeed>> getProfileFeedList(@NonNull final String userId, final Date endBefore, final int pageSize);
 }
