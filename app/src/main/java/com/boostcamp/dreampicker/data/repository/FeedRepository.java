fa9fd979f463f43fed2bd0e53e8afc21be850198
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-09 22:50:31 +0900

    issue-154 파라미터 네이밍 수정

--- app/src/main/java/com/boostcamp/dreampicker/data/repository/FeedRepository.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/repository/FeedRepository.java
@@ -10,7 +10,7 @@ import io.reactivex.Single;
 public interface FeedRepository {
     // 마감되지않은 팔로워 피드 리스트 요청
     @NonNull
-    Single<List> unfinishedFollwerFeedList(int start, int display);
+    Single<List> unfinishedFollowerFeedList(int startAt, int pageSize);
 
     // 투표 반영 및 해당 투표 요청
     @NonNull
