667054ce592446f68162bb1fb4506c91a28dd682
yeseul <sdc01194@gmail.com>
2019-02-10 15:31:57 +0900

    issue-155 onSuccessListener 사용

--- app/src/main/java/com/boostcamp/dreampicker/data/repository/FeedRepositoryImpl.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/repository/FeedRepositoryImpl.java
@@ -35,7 +35,6 @@ public class FeedRepositoryImpl implements FeedRepository {
 
     @NonNull
     private final FirebaseFirestore firestore;
-    private final String COLLECTION_FEED = "feed";
     private final String COLLECTION_USER = "user";
     private final String SUBCOLLECTION_MYFEEDS = "myFeeds";
     private final String FIELD_DATE = "date";
@@ -68,26 +67,12 @@ public class FeedRepositoryImpl implements FeedRepository {
                         .startAfter(startAfter)
                         .limit(pageSize)
                         .get()
-                        .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) { // 쿼리 성공
-
-                                // result 없거나 리스트가 비어있으면 null 반환
-                                List<DocumentSnapshot> result =
-                                        task.getResult() == null || task.getResult().isEmpty()
-                                                ? null : task.getResult().getDocuments();
-
-                                if (result != null) { // 결과 존재
-                                    List<MyFeed> feedList = new ArrayList<>();
-                                    for (DocumentSnapshot document : result) {
-                                        feedList.add(document.toObject(MyFeed.class));
-                                    }
-                                    emitter.onSuccess(feedList);
-                                } else {
-                                    emitter.onError(new Throwable("result is empty"));
-                                }
+                        .addOnSuccessListener(task -> {
+                            if(!task.isEmpty()){
+                                List<MyFeed> feedList = task.toObjects(MyFeed.class);
+                                emitter.onSuccess(feedList);
                             } else {
-                                // 쿼리 실패
-                                emitter.onError(task.getException());
+                                emitter.onSuccess(null);
                             }
                         })
                         .addOnFailureListener(emitter::onError));
--- app/src/main/java/com/boostcamp/dreampicker/data/repository/UserRepositoryImpl.java
+++ app/src/main/java/com/boostcamp/dreampicker/data/repository/UserRepositoryImpl.java
@@ -1,8 +1,8 @@
 package com.boostcamp.dreampicker.data.repository;
 
 import com.boostcamp.dreampicker.data.model.UserDetail;
-import com.boostcamp.dreampicker.data.source.firestore.mapper.UserResponseMapper;
-import com.boostcamp.dreampicker.data.source.firestore.response.UserDetailResponse;
+import com.boostcamp.dreampicker.data.source.firestore.mapper.UserDetailMapper;
+import com.boostcamp.dreampicker.data.source.firestore.model.UserDetailEntity;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.annotation.NonNull;
@@ -37,22 +37,12 @@ public class UserRepositoryImpl implements UserRepository {
         return Single.create(emitter ->
                 firestore.collection(COLLECTION_USER).document(userId)
                         .get()
-                        .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) { // 쿼리 성공
-
-                                // result 없거나, document 비었을 때 null 반환
-                                final UserDetailResponse response =
-                                        task.getResult() == null || !task.getResult().exists()
-                                        ? null : task.getResult().toObject(UserDetailResponse.class);
-
-                                if (response != null) { // 결과 존재
-                                    emitter.onSuccess(UserResponseMapper.toUserDetail(response));
-                                } else { // 결과 없음
-                                    emitter.onError(new Throwable("result is empty"));
-                                }
+                        .addOnSuccessListener(task -> {
+                            if(task.exists()){
+                                final UserDetailEntity response = task.toObject(UserDetailEntity.class);
+                                emitter.onSuccess(response == null ? null : UserDetailMapper.toUserDetail(response));
                             } else {
-                                // 쿼리 실패
-                                emitter.onError(task.getException());
+                                emitter.onSuccess(null);
                             }
                         })
                         .addOnFailureListener(emitter::onError));
