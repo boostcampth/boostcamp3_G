cce2d49a34e30ff97b7a902faa991e7a06e6c2ed
leedaegun <lkg1104@naver.com>
2019-02-12 11:54:40 +0900

    feature/issue-170-mod-upload-query MyFeedRemoteData 생성

---
+++ app/src/main/java/com/boostcamp/dreampicker/data/source/firebase/model/MyFeedRemoteData.java
@@ -0,0 +1,48 @@
+package com.boostcamp.dreampicker.data.source.firebase.model;
+
+import java.util.Date;
+
+public class MyFeedRemoteData {
+
+    private String content;
+    private Date date;
+    private String imageUrlA;
+    private String imageUrlB;
+    private boolean isEnded;
+
+    public MyFeedRemoteData() {
+    }
+
+    public MyFeedRemoteData(String content,
+                            Date date,
+                            String imageUrlA,
+                            String imageUrlB,
+                            boolean isEnded) {
+
+        this.content = content;
+        this.date = date;
+        this.imageUrlA = imageUrlA;
+        this.imageUrlB = imageUrlB;
+        this.isEnded = isEnded;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getImageUrlA() {
+        return imageUrlA;
+    }
+
+    public String getImageUrlB() {
+        return imageUrlB;
+    }
+
+    public boolean isEnded() {
+        return isEnded;
+    }
+}
