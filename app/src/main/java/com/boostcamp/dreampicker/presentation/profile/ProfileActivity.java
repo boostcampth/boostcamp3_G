f8f7f3070f60cb427e2bfa0401f1ce838598167b
yeseul <sdc01194@gmail.com>
2019-02-14 16:03:11 +0900

    issue-214 finish 수정

--- app/src/main/java/com/boostcamp/dreampicker/presentation/profile/ProfileActivity.java
+++ app/src/main/java/com/boostcamp/dreampicker/presentation/profile/ProfileActivity.java
@@ -83,7 +83,7 @@ public class ProfileActivity extends BaseActivity<ActivityProfileBinding> {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
+            finish();
         }
         return super.onOptionsItemSelected(item);
     }
