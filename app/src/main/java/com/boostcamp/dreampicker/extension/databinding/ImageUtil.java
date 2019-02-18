94f06423da550d7a0f028858cf2b6d779bef9d38
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-18 18:55:20 +0900

    issue-255 placeholder 수정 및 레이아웃 최적화

--- app/src/main/java/com/boostcamp/dreampicker/extension/databinding/ImageUtil.java
+++ app/src/main/java/com/boostcamp/dreampicker/extension/databinding/ImageUtil.java
@@ -1,6 +1,5 @@
 package com.boostcamp.dreampicker.extension.databinding;
 
-import android.net.Uri;
 import android.widget.ImageView;
 
 import com.boostcamp.dreampicker.R;
@@ -13,23 +12,6 @@ import androidx.databinding.BindingAdapter;
 public class ImageUtil {
 
     @BindingAdapter({"image"})
-    public static void loadImage(ImageView imageView, Uri uri) {
-
-        if (uri == null) {
-            GlideApp.with(imageView)
-                    .load(R.drawable.ic_add_to_photos)
-                    .into(imageView);
-        } else {
-            GlideApp.with(imageView)
-                    .load(uri)
-                    .transform(new RoundedCorners(20))
-                    .error(R.drawable.ic_broken_image_black)
-                    .transition(DrawableTransitionOptions.withCrossFade())
-                    .into(imageView);
-        }
-    }
-
-    @BindingAdapter({"image"})
     public static void loadImage(ImageView imageView, String url) {
         if (url == null) {
             GlideApp.with(imageView)
@@ -39,7 +21,7 @@ public class ImageUtil {
             GlideApp.with(imageView)
                     .load(url)
                     .transform(new RoundedCorners(20))
-                    .placeholder(R.drawable.ic_photo)
+                    .placeholder(R.drawable.bg_placeholder_gray)
                     .error(R.drawable.ic_broken_image_black)
                     .transition(DrawableTransitionOptions.withCrossFade())
                     .into(imageView);
@@ -51,13 +33,13 @@ public class ImageUtil {
     public static void rectangleLoadImage(ImageView imageView, String url) {
         if (url == null) {
             GlideApp.with(imageView)
-                    .load(R.drawable.ic_photo)
+                    .load(R.drawable.bg_placeholder_gray)
                     .into(imageView);
         } else {
             GlideApp.with(imageView)
                     .load(url)
-                    .placeholder(R.drawable.ic_photo)
-                    .error(R.drawable.ic_photo)
+                    .placeholder(R.drawable.bg_placeholder_gray)
+                    .error(R.drawable.bg_placeholder_gray)
                     .transition(DrawableTransitionOptions.withCrossFade())
                     .into(imageView);
         }
@@ -67,12 +49,13 @@ public class ImageUtil {
     public static void loadCircleImage(ImageView imageView, String url) {
         if (url == null) {
             GlideApp.with(imageView)
-                    .load(R.drawable.ic_photo)
+                    .load(R.drawable.ic_person_gray)
                     .into(imageView);
         } else {
             GlideApp.with(imageView)
                     .load(url)
                     .circleCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
                     .into(imageView);
         }
     }
