f390322481731a587ee44c94e240c65e8ec96942
tbtzpdlql <tbtzpdlql@gmail.com>
2019-02-20 10:23:45 +0900

    issue-263 SelectionGroup v1.0

--- app/src/main/java/com/boostcamp/dreampicker/utils/SelectionGroup.java
+++ app/src/main/java/com/boostcamp/dreampicker/utils/SelectionGroup.java
@@ -1,16 +1,29 @@
 package com.boostcamp.dreampicker.utils;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.ImageView;
 
 import com.boostcamp.dreampicker.R;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import androidx.annotation.DrawableRes;
 import androidx.annotation.IdRes;
+import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.Guideline;
 
@@ -20,6 +33,14 @@ import static androidx.constraintlayout.widget.ConstraintLayout.LayoutParams.VER
 import static androidx.constraintlayout.widget.ConstraintLayout.LayoutParams.WRAP_CONTENT;
 
 public class SelectionGroup extends ConstraintLayout {
+    private static final int NONE = 0, LEFT = 1, RIGHT = 2;
+    private static final String AUTO_TAG = "AUTO_TAG";
+
+    @IntDef({NONE, LEFT, RIGHT})
+    @Retention(RetentionPolicy.SOURCE)
+    private @interface Position {
+    }
+
     private int GUIDELINE;
     private int CONTAINER_LEFT;
     private int CONTAINER_RIGHT;
@@ -29,8 +50,22 @@ public class SelectionGroup extends ConstraintLayout {
 
     private int index = 0;
 
+    private String tag;
+
     public ImageView left, right;
     public View selector;
+
+    @Nullable
+    private OnDropListener onDropListener;
+    private OnTouchListener onTouchListener = new OnTouchListener();
+
+    private interface OnDropListener {
+        void onDrop(@Position final int position);
+    }
+
+    private interface OnDropCallback {
+        void onDrop();
+    }
 
     public SelectionGroup(Context context) {
         super(context);
@@ -53,7 +88,8 @@ public class SelectionGroup extends ConstraintLayout {
         initViewId();
         initGuideline();
         initContainer();
-        createSelector();
+        initSelector();
+        initListener();
     }
 
     private void initViewId() {
@@ -98,7 +134,6 @@ public class SelectionGroup extends ConstraintLayout {
         params.startToStart = startToStart;
         params.endToEnd = endToEnd;
 
-
         imageView.setScaleType(ImageView.ScaleType.FIT_XY);
         imageView.setAdjustViewBounds(true);
         imageView.setLayoutParams(params);
@@ -106,7 +141,7 @@ public class SelectionGroup extends ConstraintLayout {
         return imageView;
     }
 
-    private void createSelector() {
+    private void initSelector() {
         selector = new View(getContext());
 
         final int size = dp2px(DEFAULT_SIZE);
@@ -123,15 +158,94 @@ public class SelectionGroup extends ConstraintLayout {
         addView(selector, getIndex());
     }
 
-    public void setSelectorSize(int size) {
+    private void initListener() {
+        selector.setOnTouchListener(onTouchListener);
+        left.setOnDragListener(new OnDragListener(() -> {
+            dropAnimation(LEFT);
+            if (onDropListener != null) {
+                onDropListener.onDrop(LEFT);
+            }
+        }));
+
+        right.setOnDragListener(new OnDragListener(() -> {
+            dropAnimation(RIGHT);
+            if (onDropListener != null) {
+                onDropListener.onDrop(RIGHT);
+            }
+        }));
+    }
+
+    private void dropAnimation(@Position final int position) {
+        selector.startAnimation(new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                final ConstraintLayout.LayoutParams params =
+                        (ConstraintLayout.LayoutParams) selector.getLayoutParams();
+
+                if (position == NONE) {
+                    params.startToStart = PARENT_ID;
+                    params.endToEnd = PARENT_ID;
+                } else if (position == LEFT) {
+                    params.startToStart = PARENT_ID;
+                    params.endToEnd = GUIDELINE;
+                } else if (position == RIGHT) {
+                    params.startToStart = GUIDELINE;
+                    params.endToEnd = PARENT_ID;
+                }
+                selector.setLayoutParams(params);
+            }
+        });
+    }
+
+    public void dropLeft() {
+        dropAnimation(LEFT);
+    }
+
+    public void dropRight() {
+        dropAnimation(RIGHT);
+    }
+
+    public void dropCancel() {
+        dropAnimation(NONE);
+    }
+
+    /**
+     * Apply selector size.
+     */
+    private void setSelectorSize(int size) {
         final LayoutParams params = (LayoutParams) selector.getLayoutParams();
         final int dpSize = dp2px(size);
         params.width = dpSize;
         params.height = dpSize;
     }
 
-    public void setSelectorBackgroundResource(@DrawableRes int resId) {
+    /**
+     * Apply selector background image by resource ID.
+     */
+    private void setSelectorBackgroundResource(@DrawableRes int resId) {
         selector.setBackgroundResource(resId);
+    }
+
+    /**
+     * @param data Send clip data to drop view.
+     */
+    public void setData(@NonNull final String data) {
+        onTouchListener.setData(data);
+    }
+
+    /**
+     * This params decide drop range.
+     * If "SelectionGroups" are same tag, There are possible to dropdown.
+     */
+    public void setTag(@NonNull final String tag) {
+        this.tag = tag;
+    }
+
+    /**
+     * Selector dropdown listener at outer.
+     */
+    public void setOnDropListener(@Nullable OnDropListener onDropListener) {
+        this.onDropListener = onDropListener;
     }
 
     private void getAttrs(AttributeSet attrs) {
@@ -158,6 +272,10 @@ public class SelectionGroup extends ConstraintLayout {
         int size = typedArray.getResourceId(R.styleable.SelectionGroup_selectorSize, DEFAULT_SIZE);
         setSelectorSize(size);
 
+        final String tag = typedArray.getString(R.styleable.SelectionGroup_selectorTag);
+        this.tag = tag == null ? AUTO_TAG : tag;
+        onTouchListener.setLabel(this.tag);
+
         typedArray.recycle();
     }
 
@@ -167,5 +285,73 @@ public class SelectionGroup extends ConstraintLayout {
 
     private int dp2px(float dp) {
         return (int) (dp * getContext().getApplicationContext().getResources().getDisplayMetrics().density + 0.5f);
+    }
+
+    private class OnTouchListener implements View.OnTouchListener {
+        private String DEFAULT_DATA = "";
+        private String label;
+        private String data;
+
+        private void setLabel(@NonNull final String label) {
+            this.label = label;
+        }
+
+        private void setData(@NonNull final String data) {
+            this.data = data;
+        }
+
+        @SuppressLint("ClickableViewAccessibility")
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                if (data == null) {
+                    data = DEFAULT_DATA;
+                }
+                final ClipData.Item item = new ClipData.Item(data);
+                final String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+                final ClipData clipData = new ClipData(label, mimeTypes, item);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    v.startDragAndDrop(clipData, // Data to be dragged
+                            new View.DragShadowBuilder(v), // Drag shadow
+                            v, // Dropdown  Vew
+                            0 // No operation
+                    );
+                } else {
+                    v.startDrag(clipData, // Data to be dragged
+                            new View.DragShadowBuilder(v), // Drag shadow
+                            v, // Dropdown view
+                            0 // No operation
+                    );
+                }
+                // If touch selector, it is INVISIBLE state.
+                // After dropdown, Back VISIBLE state.
+                v.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    private class OnDragListener implements View.OnDragListener {
+        @NonNull
+        private final OnDropCallback callback;
+
+        private OnDragListener(@NonNull OnDropCallback callback) {
+            this.callback = callback;
+        }
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            if (event.getAction() == DragEvent.ACTION_DROP) {
+                final CharSequence label = event.getClipData().getDescription().getLabel();
+                if (label != null && label.toString().equals(tag)) callback.onDrop();
+                else return false;
+            } else if (event.getAction() == DragEvent.ACTION_DRAG_ENDED) {
+                selector.setVisibility(View.VISIBLE);
+            }
+            return true;
+        }
     }
 }
